d64a89591825614c28395c2c75c046812f2e0820
aapkajay <jaykumardohare123@gmail.com>
2025-05-18 14:10:41 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://payout-automation.onrender.com";
 
 export async function fetchMentors() {
   const res = await axios.get(`${BASE_URL}/mentors`);
